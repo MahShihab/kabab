93c3eda339f8065c27fa3aceab0d204a950c0215
Mahmoud <sandi.pc.man@gmail.com>
2022-11-21 14:14:35 +0200

    createDatabaseData

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from os import path
+from sqlalchemy.sql import text 
 import stripe
 
 login_manager= LoginManager()
@@ -41,4 +42,39 @@ def create_app():
 def create_database(app):
     if not path.exists('website/'+DB_Name):
         db.create_all(app=app)
+        
+        sql_file = open('website/SQL.txt','r')
+
+        # Create an empty command string
+        sql_command = ''
+        
+        # Iterate over all lines in the sql file
+        for line in sql_file:
+            # Ignore commented lines
+            if not line.startswith('--') and line.strip('\n'):
+                # Append line to the command string
+                sql_command += line.strip('\n')
+        
+                # If the command string ends with ';', it is a full statement
+                if sql_command.endswith(';'):
+                    # Try to execute statement and commit it
+                    print(sql_command)
+                    try:
+                        sql = text(sql_command)
+                        # db.engine.execute(sql)
+                        db.session.execute(sql)
+                        print("meaw")
+                        db.session.commit()
+        
+                    # Assert in case of error
+                    except:
+                        print('Ops')
+        
+                    # Finally, clear command string
+                    finally:
+                        sql_command = ''
+
+
+
+
         print('Created Database')    
