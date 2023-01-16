c8c50feb73247e01c1f2e7e2e4617aca2b22276b
MahShihab <sandi.pc.man@gmail.com>
2023-01-16 13:48:05 +0200

    test3

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,redirect,url_for,request,flash
+from flask import Blueprint,render_template,redirect,url_for,request,flash, current_app
 from . import login_manager,db
 # from werkzeug.security import generate_password_hash , check_password_hash
 from .models import Admin,Order,OrderLine,Item
@@ -14,11 +14,15 @@ def login():
         admin = Admin.query.filter_by(Password=Apass).first()
         
         flash(f'in line 16, {admin}', category='warn') 
+        
+        print(f'\n\n\nLINE16 {admin}')
+        current_app.logger.error(f'LINE16 -- {admin}')
 
         if admin :
             flash('Logged successfully', category='success')
             out = login_user(admin,remember=True)
             flash(f'in line 21, {out}', category='warn') 
+            current_app.logger.error(f'LINE25 -- {out}')
             return render_template("login.html",user=current_user)
 
 
@@ -37,5 +41,6 @@ def logout():
 
 @login_manager.unauthorized_handler
 def unauthorized():
+    flash('unauthorized','error')
     return redirect(url_for('auth.login'))
 
